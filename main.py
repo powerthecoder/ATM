68c59e3154db8ab551e8e553e0dd190cb3489aea
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2020-08-04 00:07:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,47 @@ class Main():
         option = int(input("Option > "))
         
         if (option == 1):
-            amount = float(input("Amount to deposit $"))
-            if (amount > 0):
-                try:
-                    print(f"Depositing ${amount}")
-                    self.Deposit(amount)
-                except Exception as e:
-                    print(f"[ERROR] > {e}")
+            x = 0
+            while (x != 1):
+                amount = float(input("Amount to deposit $"))
+                if (amount > 0):
+                    try:
+                        print(f"Depositing ${amount}")
+                        self.Deposit(amount)
+                        x += 1
+                    except Exception as e:
+                        print(f"[ERROR] > {e}")
+                else:
+                    print(f"Can not deposit ${amount}")
+        
+        elif (option == 2):
+            x = 0
+            while (x != 1):
+                amount = float(input("Amount to withdraw $"))
+                if (amount > 0):
+                    try:
+                        with open("balance.json", "r") as f:
+                            data = json.load(f)
+                        total = data['total']
+                        if (amount < float(total)):
+                            print(f"Withdrawing ${amount}")
+                            self.Withdraw(amount)
+                            x += 1
+                        else:
+                            print(f"Not enough funds in account to withdraw ${amount}")
+                    except Exception as e:
+                        print(f"[ERROR] > {e}")
+                else:
+                    print(f"Can not withdraw ${amount}")
 
+        elif (option == 3):
+            self.Balance()
+        
+        else:
+            print("Unknown Argument...")
+            print("Shutting Down")
+            time.sleep(1)
+            sys.exit()
     
     def Deposit(self, amount):
         try:
@@ -41,13 +74,23 @@ class Main():
 
     def Withdraw(self, amount):
         try:
-            pass
+            with open("balance.json", "r") as f:
+                data = json.load(f)
+            total = data['total']
+            new_total = float(total) - float(amount)
+            print(f"New Total: ${new_total}")
+            data['total'] = float(new_total)
+            with open("balance.json", "w") as f:
+                json.dump(data, f)
         except Exception as e:
             print(f"[ERROR] > {e}")
 
     def Balance(self):
         try:
-            pass
+            with open("balance.json", "r") as f:
+                data = json.load(f)
+            total = data['total']
+            print(f"Your Total is ${total}")
         except Exception as e:
             print(f"[ERROR] > {e}")
 
